286370d6c241af986484a05158d387568c7400bd
António Ferreira <antonio.ferreira@deptagency.com>
2019-10-09 16:51:53 +0200

    Adds fix for accessibility

--- src/index.js
+++ src/index.js
@@ -10,3 +10,23 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// accessibility fix
+
+function handleFirstTab(e) {
+    if (e.keyCode === 9) {
+        document.body.classList.add('user-is-tabbing');
+
+        window.removeEventListener('keydown', handleFirstTab);
+        window.addEventListener('mousedown', handleMouseDownOnce);
+    }
+}
+
+function handleMouseDownOnce() {
+    document.body.classList.remove('user-is-tabbing');
+
+    window.removeEventListener('mousedown', handleMouseDownOnce);
+    window.addEventListener('keydown', handleFirstTab);
+}
+
+window.addEventListener('keydown', handleFirstTab);
